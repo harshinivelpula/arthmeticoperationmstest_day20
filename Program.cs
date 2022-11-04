c4a114c74b74dd9132ddf470d16f648bce6284c0
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-04 20:38:40 +0530

    adding main

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿namespace ArthmeticOperation
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Add add = new Add();
+            Add.ReferenceEquals(add, add);
+
+            UnitTest1 unitTest1 = new UnitTest1();
+            UnitTest1.ReferenceEquals(unitTest1, add);
+        }
+    }
+
+    internal class UnitTest1
+    {
+    }
+}
