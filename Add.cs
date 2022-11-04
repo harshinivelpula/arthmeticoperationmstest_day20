22c227911f131dfcb6e9f4dc1250fd4686da5996
DELL <105187636+harshinivelpula@users.noreply.github.com>
2022-11-04 20:31:39 +0530

    add

---
+++ Add.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArthmeticOperation
+{
+    internal class Add
+    {
+        public int Sum(int a, int b)
+        {
+            if (a == 0) 
+            {
+                return b;
+            }
+            else if (b == 0) 
+                {
+                    return a;
+                }
+            else {
+                int c = a + b;
+
+                return c;
+            }
+        }
+    }
+}
